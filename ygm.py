be81fcf8a0d20c8f588487d1cf6decc6ad3836f8
Chris Lyon <chris@cplyon.ca>
2023-11-24 22:13:34 -0800

    Simplified code and improved logging

--- ygm.py
+++ ygm.py
@@ -20,76 +20,61 @@ from RPi import GPIO
 
 class YouveGotMail:
     def __init__(self):
-        self.account = None
-        self.password = None
-        self.port = None
-        self.server = None
-        self.to_addresses = []
-        self.from_address = None
-        self.subject = None
-        self.message = None
-        self.brightness = None
-        self.switch_pin = None
-        self.image_location = None
+        self.config = None
+        logging.info("Starting up")
 
     def setup_gpio(self):
+        logging.info("Setting up GPIO")
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.config.switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def wait_for_switch_open(self):
-        GPIO.wait_for_edge(self.switch_pin, GPIO.RISING)
-        logging.warning("Door opened")
+        GPIO.wait_for_edge(self.config.switch_pin, GPIO.RISING)
+        logging.debug("Door opened")
 
     def wait_for_switch_close(self):
-        GPIO.wait_for_edge(self.switch_pin, GPIO.FALLING)
-        logging.warning("Door closed")
+        GPIO.wait_for_edge(self.config.switch_pin, GPIO.FALLING)
+        logging.debug("Door closed")
 
     def take_photo(self) -> str:
         image_path = os.path.join(
-            self.image_location,
+            self.config.image_location,
             f"image_{datetime.now().strftime('%b_%d_%Y_%H_%M_%S')}.jpg",
         )
+        logging.info("Taking photo")
         with PiCamera() as camera:
-            camera.brightness = self.brightness
+            camera.brightness = self.config.brightness
+            logging.debug("Saving photo to %s", image_path)
             camera.capture(image_path)
         return image_path
 
     def compose_email(self, attachment_path: str) -> str:
         msg = MIMEMultipart()
-        msg["From"] = self.from_address
-        msg["To"] = COMMASPACE.join(self.to_addresses)
+        msg["From"] = self.config.from_address
+        msg["To"] = COMMASPACE.join(self.config.to_addresses)
         msg["Date"] = formatdate(localtime=True)
-        msg["Subject"] = self.subject
-        msg.attach(MIMEText(self.message))
+        msg["Subject"] = self.config.subject
+        msg.attach(MIMEText(self.config.message))
 
         with open(attachment_path, "rb") as image_file:
             img = MIMEImage(image_file.read(), "jpg")
-            img.add_header("Content-ID", "<image>")
-            img.add_header("Content-Disposition", "inline", filename="image.jpg")
-            msg.attach(img)
+        img.add_header("Content-ID", "<image>")
+        img.add_header("Content-Disposition", "inline", filename="image.jpg")
+        msg.attach(img)
 
         return msg.as_string()
 
-    def send_email(self, msg: str) -> None:
+    def send_email(self, msg: str):
+        logging.info("Sending email")
         context = ssl.create_default_context()
-        with smtplib.SMTP(self.server, self.port) as server:
+        with smtplib.SMTP(self.config.server, self.config.port) as server:
             server.starttls(context=context)
-            server.login(self.account, self.password)
-            server.sendmail(self.from_address, self.to_addresses, msg)
+            server.login(self.config.account, self.config.password)
+            server.sendmail(self.config.from_address, self.config.to_addresses, msg)
 
     def read_config(self, file_stream: StringIO):
-        config = json.load(file_stream)
-        self.account = config["account"]
-        self.password = config["password"]
-        self.server = config["server"]
-        self.port = config["port"]
-        self.from_address = config["from"]
-        self.to_addresses = config["to"]
-        self.subject = config["subject"]
-        self.message = config["message"]
-        self.brightness = config["brightness"]
-        self.switch_pin = config["switch_pin"]
-        self.image_location = config["image_location"]
+        logging.info("Reading config")
+        self.config = json.load(file_stream)
 
 
 def main():
@@ -102,7 +87,6 @@ def main():
         ygm.wait_for_switch_open()
         time.sleep(DOOR_SLEEP_SECONDS)
         image = ygm.take_photo()
-        logging.debug(image)
         message = ygm.compose_email(image)
 
         retry_counter = 0
@@ -111,11 +95,11 @@ def main():
             try:
                 ygm.send_email(message)
             except (gaierror, smtplib.SMTPServerDisconnected, OSError) as error:
-                logging.debug(error)
+                logging.error(error)
                 if retry_counter >= MAX_RETRIES:
-                    logging.debug("%s retries reached, abort sending mail", MAX_RETRIES)
+                    logging.error("%s retries reached, abort sending mail", MAX_RETRIES)
                     break
-                logging.debug("Mail failed to send, retrying in %s", MAIL_SEND_SLEEP_SECONDS)
+                logging.warning("Mail failed to send, retrying in %s", MAIL_SEND_SLEEP_SECONDS)
                 time.sleep(MAIL_SEND_SLEEP_SECONDS)
                 retry_counter += 1
             else:
@@ -129,12 +113,12 @@ if __name__ == "__main__":
     MAIL_SEND_SLEEP_SECONDS = 60
     DOOR_SLEEP_SECONDS = 10
     MAX_RETRIES = 2
-    LOG_NAME = "ygm.log"
+    LOG_FILE_NAME = "ygm.log"
 
-    logging.basicConfig(filename=LOG_NAME,
+    logging.basicConfig(filename=LOG_FILE_NAME,
                     filemode='a',
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S',
-                    level=logging.DEBUG)
+                    level=logging.INFO)
 
     main()
